9b6cff9c8fef5b1da126ea063c1a513df57161c0
Neeschal1 <neeschalpok04@gmail.com>
2025-08-31 18:42:55 +0545

    created a simple web app for this chatbot app, using Streamlit

---
+++ chat.py
@@ -0,0 +1,29 @@
+#output in web app through streamlit
+
+import streamlit as st
+import pyttsx3
+import os
+import google.generativeai as genai
+from dotenv import load_dotenv
+
+load_dotenv()
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+genai.configure(api_key=SECRET_KEY)
+model_of_AI = genai.GenerativeModel('gemini-2.5-flash')
+
+st.title("Hello, World!")
+st.text("Welcome to Chatty :)")
+
+engine = pyttsx3.init()
+
+def user():
+    user_input = st.text_input("Enter anything to ask to Neeschal's AI:")
+    return user_input
+    
+ask_ai = user()
+    
+if st.button("Send"):
+       response = model_of_AI.generate_content(ask_ai)
+       st.write(response.text)
+   
--- main.py
+++ main.py
@@ -1,20 +1,27 @@
+# output in terminal
+
 import google.generativeai as genai
 from dotenv import load_dotenv
 import os
 import pyttsx3
+import streamlit as st
 
 load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
+st.title("Hello, World!")
+st.text("Welcome to Chatty :)")
+
 engine = pyttsx3.init()
-terminated_response = "Byebye. Take Care. See you soon :) \n"
+# terminated_response = "Byebye. Take Care. See you soon :) \n"
 
 def decide():
     decision = int(input('''\nPress 1 to continue, and press 2 to terminate the program: '''))
     func(decision)
 
 def loop():
-    user_input = input("\nEnter anything: ")
+    # user_input = input("\nEnter anything: ")
+    user_input = st.text_input("\nEnter anything: ")
     try: 
        response = model_of_AI.generate_content(user_input)
        engine.say(response.text)
@@ -28,8 +35,8 @@ def func(x):
         loop()
         decide()
     else:
-        engine.say(terminated_response)
-        print(terminated_response)
+        # engine.say(terminated_response)
+        # print(terminated_response)
         engine.runAndWait()
         pass
     
